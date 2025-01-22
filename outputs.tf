8024e738ac79c9a9be86f44d2152724536b9d07d
Remeziz <zelinskiynikol@gmail.com>
2025-01-22 12:50:42 +0200

    json

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,4 @@
-output "asg_name_1" {
-  value       = aws_autoscaling_group.asg.name
-  description = "Name of the first Auto Scaling Group"
-}
-
-output "asg_name_2" {
-  value       = aws_autoscaling_group.asg2.name
-  description = "Name of the second Auto Scaling Group"
+output "asg_names" {
+  value       = [aws_autoscaling_group.asg.name, aws_autoscaling_group.asg2.name]
+  description = "Names of the Auto Scaling Groups"
 }
